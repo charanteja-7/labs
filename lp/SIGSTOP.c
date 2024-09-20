94c3c56f429618bce1bdac87bbc0a8fcb16f9383
Charan Teja Chukkala <149792997+charanteja-7@users.noreply.github.com>
2024-09-20 11:10:06 +0530

    Update and rename SIGSTOP to SIGSTOP.c

--- lp/SIGSTOP.c
+++ lp/SIGSTOP.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -7,8 +6,11 @@
 int main() {
     printf("Running... (Send SIGSTOP using `kill -STOP <pid>`)\n");
     while (1) {
-        printf("Still running... (Press Ctrl+C to stop)\n");
+         printf("Still running... (PID: %d) (Press Ctrl+C to stop)\n", getpid());
         sleep(1);
     }
     return 0;
 }
+
+//use another terminal to stop this process using the pid
+//kill -STOP <pid>
