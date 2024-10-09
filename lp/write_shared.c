9308b8403c52b838b4a9f8a43ec2898a2c10090e
Charan Teja Chukkala <149792997+charanteja-7@users.noreply.github.com>
2024-10-09 09:29:51 +0530

    Create write_shared.c

---
+++ lp/write_shared.c
@@ -0,0 +1,29 @@
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+int main(){
+	char buff[1024];
+	int shmid;
+	void *shm_addr;
+	//create a shared memory
+	shmid = shmget(1124, 1024, 0666|IPC_CREAT);
+	if(shmid == -1){
+		printf("Error in creating shared memory");
+		exit(1);
+	}
+	//attach the shared memory to address space
+	shm_addr = shmat(shmid , NULL, 0);
+	read(0, buff, 1024);
+	strcpy(shm_addr, buff);
+	printf("data written at %p  is %s ",shm_addr, (char * )shm_addr);
+	shmdt(shm_addr);
+	printf("\n");
+	return 0;
+}
